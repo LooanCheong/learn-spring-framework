4128a0bbcbaff78d55330a2e7bb73a8a9551ec46
LooanCheong <jjyejun@naver.com>
2023-09-25 23:45:05 +0900

    MarioGame 생성, 기능 추가

---
+++ src/main/java/com/Looan/learnspringframework/game/MarioGame.java
@@ -0,0 +1,20 @@
+package com.Looan.learnspringframework.game;
+
+public class MarioGame {
+
+	public void up() {
+		System.out.println("Jump");
+	}
+
+	public void down() {
+		System.out.println("Slide");
+	}
+
+	public void left() {
+		System.out.println("Left");
+	}
+
+	public void right() {
+		System.out.println("Right");
+	}
+}
