fc1af17f94d3c2774ebd9612f6095c8dd4380323
LooanCheong <jjyejun@naver.com>
2023-10-01 04:24:05 +0900

    프라이빗 리소스

--- src/main/java/com/Looan/learnspringframework/helloworld/App02HellowWorldSpring.java
+++ src/main/java/com/Looan/learnspringframework/helloworld/App02HellowWorldSpring.java
@@ -8,20 +8,24 @@ public class App02HellowWorldSpring {
 
 	public static void main(String[] args) {
 		// Spring Context 실행
-
-		var context = new AnnotationConfigApplicationContext(HelloWorldConfiguration.class);
-
 		// Spring Framwork가 관리하도록 설정
 		// HelloWorldConfiguration - @Configuration
 		// name - @Bean
-		System.out.println(context.getBean("name"));
-		System.out.println(context.getBean("age"));
-		System.out.println(context.getBean("person"));
-		System.out.println(context.getBean("person2"));
-		System.out.println(context.getBean("person3"));
-		System.out.println(context.getBean("address2"));
-		System.out.println(context.getBean(Address.class));
+		try(var context = new AnnotationConfigApplicationContext(HelloWorldConfiguration.class)){
+			System.out.println(context.getBean("name"));
+			System.out.println(context.getBean("age"));
+			System.out.println(context.getBean("person"));
+			System.out.println(context.getBean("person2"));
+			System.out.println(context.getBean("person3"));
+			System.out.println(context.getBean("address2"));
+			System.out.println(context.getBean(Address.class));
 
-		Arrays.stream(context.getBeanDefinitionNames()).forEach(System.out::println);
+			Arrays.stream(context.getBeanDefinitionNames()).forEach(System.out::println);
+			
+		}
+		};
+
+		
+		
 	}
 }
